eeadd6c0ee99d79159ecdbc3e9bbbc2a4f4d0c8b
Luc Anthony Nkounkou <luc-anthony.nkounkou@stat.gouv.qc.ca>
2022-09-15 23:53:16 -0400

    put all of the pieces of the algorithm toghether. The whole logic works but still need to fix some bugs for matching optimization. Some objects (csv & docFile) but other cannot match because of more possibilities of matches because there is no match at all.

--- matchingLogic.js
+++ matchingLogic.js
@@ -2,6 +2,7 @@
 //output => same object with 1 new key & value pairs
 //input comes from testmain.js
 //import { getCsvData } from "./testmain.js";
+import { createObjectCsvWriter } from 'csv-writer';
 import { readFilesFromDir } from "./refactorDocFileExtract.js";
 import { outPutDocFiles } from "./docFileExtract.js";
 import { getCsvData } from "./testmain.js";
@@ -9,15 +10,12 @@ import { extractDataFromFile } from "./refactorDocFileExtract.js";
 const DOC_FILE_DIR = "C:/Users/stat2071/lib/Algorithms-data/node-parser/Diffusee";
 const CSV_FILE_NAME = "C:/Users/stat2071/lib/Algorithms-data/node-parser/elevePPS01.csv";
 
-const firstCsvRow = {
-  "Variable": "Année scolaire",
-  "Description de la variable": "L’année scolaire désigne la période au cours de laquelle ont lieu les activités d’apprentissage. Elle est représentée par une année de début et une année de fin "
-};
-
 //the function finds the possible docs that can match a row
 //the role of this function is to find the match one csv row with its matching file data
-// the goal is to match one csv row with a DocFile object
+//the goal is to match one csv row with a DocFile object
 //it takes two arguments one csvRow & allDocFilesData list of objects
+//THE FUNCTION RETURN AN ARRAY OF OBJECTS : if there is exactly 1 match it returns an array of 1 docFileObject 
+//IF there is no match it return an empty & if there is more than 1 match it returns an array of all possible matches
 function possibleDocsForRow(csvRow, allDocFilesData) {
   const arrDocs = [];
   for (const docFileData of allDocFilesData) {
@@ -28,6 +26,7 @@ function possibleDocsForRow(csvRow, allDocFilesData) {
   return arrDocs;
 }
 
+
 //this function is using the above function possibleDocsForRow
 // it uses the above function for finding matches between docs & all csv rows
 // we use fileNameData to identify the doc fileName when error occurs
@@ -35,18 +34,36 @@ async function macthAllCsvRows(csvFileName, docFileDir) {
   const files = await readFilesFromDir(docFileDir);
   const docFiles = await outPutDocFiles(files, docFileDir);
   const csvRows = await getCsvData(csvFileName);
-  const fileNameData = await extractDataFromFile(fileName, dir);
   const newCsvRows = [];
   for (const csvRow of csvRows) {
     const docsForRow = possibleDocsForRow(csvRow, docFiles);
-    if (docsForRow.length != 1) {
-      console.log(`Unexpected number of matching docs for this row : ${docsForRow}`);
-      continue;
+    if (docsForRow.length > 1) {
+      console.log("There is more than 1 match for this row :", { variable: csvRow.Variable });
+      newCsvRows.push({...csvRow, "code": ""});
+    } else if (docsForRow.length == 0) {
+      console.log("There is No  match for this row :", { variable: csvRow.Variable });
+      newCsvRows.push({...csvRow, "code": ""});
+    } else {
+      newCsvRows.push({ ...csvRow, "code": docsForRow[0].code });
     }
-    newCsvRows.push({ ...csvRow, "code": docsForRow[0].code });
   }
   return newCsvRows;
-
 }
 
-console.log(await macthAllCsvRows(CSV_FILE_NAME, DOC_FILE_DIR));
+
+function writeMatches(records) {
+
+  const csvWriter = createObjectCsvWriter({
+    path: './out.csv',
+    header: [
+      { id: 'Variable', title: 'VARIABLE' },
+      { id: 'Description de la variable', title: 'DESCRIPTION DE LA VARIABLE' },
+      { id: 'code', title: 'CODE' }
+    ]
+  });
+
+  return csvWriter.writeRecords(records)
+
+}
+const newCsvRows = (await macthAllCsvRows(CSV_FILE_NAME, DOC_FILE_DIR));
+await writeMatches(newCsvRows);
